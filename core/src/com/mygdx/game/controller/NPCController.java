c9280734129710b1c0b68b5c91e097283515085b
jcaracciolo <jcaracciolo@itba.edu.ar>
2015-11-04 11:33:51 -0300

    Implementacion de la MenuScreen

--- core/src/com/mygdx/game/controller/NPCController.java
+++ core/src/com/mygdx/game/controller/NPCController.java
@@ -4,7 +4,7 @@ import com.mygdx.game.model.character.NPC;
 import com.mygdx.game.view.assets.NPCView;
 
 public class NPCController extends CharacterController<NPC,NPCView> {
-	
+
 	public NPCController(NPC npc, NPCView npcView) {
 		super(npc,npcView);
 	}
