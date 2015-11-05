9fb297b81a84e6029f49fefe0c74730991d6045d
traies <traies@lab72.it.itba.edu.ar>
2015-11-05 18:13:00 -0300

    Cambio de nombre de la clase PlayerManager a PlayerController.
    Ademas la movi al paquete cotroller

---
+++ core/src/com/mygdx/game/controller/PlayerController.java
@@ -0,0 +1,50 @@
+package com.mygdx.game.controller;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
+import com.mygdx.game.model.PlayerMovement;
+import com.mygdx.game.model.character.Player;
+import com.mygdx.game.view.assets.PlayerView;
+/**
+ * Clase que recibe datos del ControlProcessor acerca de lo que hizo el jugador y,
+ * en base a esto, actualiza el modelo.
+ * @author masaques
+ *
+ */
+public class PlayerController extends CharacterController<Player, PlayerView> {
+	private ControlProcessor control;
+	
+	public PlayerController(Player player,ControlProcessor control, PlayerView playerView){
+		super(player, playerView);
+		this.control = control ;
+	}
+	
+	public void control() {
+		updateModel();
+		updateView();
+	}
+	
+	public void updateView() {
+		super.updateView();
+	}
+	public void updateModel() {
+		if (!isDead()){
+			control.update();
+			PlayerMovement movement ;
+			movement = control.getPlayerMovement();
+			if (movement.getDirection().x != 0 || movement.getDirection().y != 0) {
+				getModel().move(movement.getDirection().nor(),movement.isRunning()) ;
+			}
+			else {
+				getModel().stopMoving();
+			}
+			
+			getModel().look(movement.getLookDirection());
+			
+			if (movement.isShooting()) {
+				getModel().shoot();
+			}
+		}
+		super.updateModel();
+	}
+}
